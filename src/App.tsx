47c5b2c3baab5c3a21964a3059bd82ca4d2c7f18
abhishandy <abhishek.sandy@gmail.com>
2022-12-13 03:31:31 -0500

    feat: cleanup landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
+import { IconName, library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import CPlusPlus from "./CPlusPlus";
 library.add(fab, fas);
 
 function SkillCard({ title, children }: any) {
   return (
-    <div className="rounded-xl p-4 mr-8 my-4 md:max-w-lg max-w-xs inline-block align-top shadow-green-400 shadow-lg border-green-400">
-      <div className="text-2xl font-bold text-center">{title}</div>
-      <div className="text-xl flex flex-wrap justify-between mt-4 gap-y-2">
-        {children}
-      </div>
+    <div className="rounded-xl md:max-w-lg mb-6 max-w-xs inline-block align-top">
+      <h1 className="mb-2 underline">{title}</h1>
+      <div className="flex flex-row">{children}</div>
     </div>
   );
 }
 
+const SkillWrapper = ({ children }: any) => (
+  <div className="flex-1 text-center mx-2">{children}</div>
+);
+
+const SocialLink = ({ href, icon }: { href: string; icon: IconName }) => (
+  <a
+    href={href}
+    target="_blank"
+    rel="noreferrer"
+    className="hover:text-green-400 flex-1 text-center md:text-left md:flex-none"
+  >
+    <FontAwesomeIcon icon={["fab", icon]} size="3x" />
+  </a>
+);
+
 function App() {
   return (
     <div className="w-full bg-black bg-opacity-95 text-white font-mono">
-      <div className="py-12 lg:px-72 md:px-24 px-12">
-        <p className="text-6xl">Abhishek Shandilya</p>
-        <p className="text-4xl mt-6">
+      <div className="pt-10 mb-6 lg:px-72 md:px-24 px-12">
+        <p className="text-5xl">Abhishek Shandilya</p>
+        <p className="text-3xl mt-6">
           <span className="text-green-400">@</span>abhishandy
         </p>
         <p className="mt-6 text-lg">
@@ -30,192 +44,76 @@ function App() {
           <span className="text-green-400">web-development</span> and{" "}
           <span className="text-green-400">Bitcoin</span>.
         </p>
-        <div className="mt-6 flex gap-4">
-          <a
-            href="https://github.com/abhishandy"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:text-green-400"
-          >
-            <FontAwesomeIcon icon={["fab", "github"]} size="3x" />
-          </a>
-          <a
-            href="https://twitter.com/abhishandy"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:text-green-400"
-          >
-            <FontAwesomeIcon icon={["fab", "twitter"]} size="3x" />
-          </a>
-          <a
-            href="https://linkedin.com/in/abhishandy"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:text-green-400"
-          >
-            <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
-          </a>
+        <div className="mt-6 flex w-full md:gap-4">
+          <SocialLink href="https://github.com/abhishandy" icon="github" />
+          <SocialLink href="https://twitter.com/abhishandy" icon="twitter" />
+          <SocialLink
+            href="https://www.linkedin.com/in/abhishandy/"
+            icon="linkedin"
+          />
         </div>
       </div>
-      <div className="py-12 lg:px-72 md:px-24 px-12">
-        <div className="text-5xl font-bold text-green-400">Skills</div>
+      <div className="my-12 lg:px-72 md:px-24 px-12">
+        <div className="text-3xl font-bold text-green-400 mb-4">Skills</div>
         <SkillCard title="Programming Languages">
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "python"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              viewBox="0 0 306 344.35"
-              className="h-10"
-            >
-              <path
-                fill="currentColor"
-                stroke="currentColor"
-                d="M302.107,258.262c2.401-4.159,3.893-8.845,3.893-13.053V99.14c0-4.208-1.49-8.893-3.892-13.052L153,172.175  L302.107,258.262z"
-              />
-              <path
-                fill="currentColor"
-                stroke="currentColor"
-                d="M166.25,341.193l126.5-73.034c3.644-2.104,6.956-5.737,9.357-9.897L153,172.175L3.893,258.263  c2.401,4.159,5.714,7.793,9.357,9.896l126.5,73.034C147.037,345.401,158.963,345.401,166.25,341.193z"
-              />
-              <path
-                fill="currentColor"
-                stroke="currentColor"
-                d="M302.108,86.087c-2.402-4.16-5.715-7.793-9.358-9.897L166.25,3.156c-7.287-4.208-19.213-4.208-26.5,0  L13.25,76.19C5.962,80.397,0,90.725,0,99.14v146.069c0,4.208,1.491,8.894,3.893,13.053L153,172.175L302.108,86.087z"
-              />
-              <g>
-                <path
-                  fill="#000"
-                  d="M153,274.175c-56.243,0-102-45.757-102-102s45.757-102,102-102c36.292,0,70.139,19.53,88.331,50.968   l-44.143,25.544c-9.105-15.736-26.038-25.512-44.188-25.512c-28.122,0-51,22.878-51,51c0,28.121,22.878,51,51,51   c18.152,0,35.085-9.776,44.191-25.515l44.143,25.543C223.142,254.644,189.294,274.175,153,274.175z"
-                />
-              </g>
-              <g>
-                <polygon
-                  fill="#000"
-                  points="255,166.508 243.666,166.508 243.666,155.175 232.334,155.175 232.334,166.508 221,166.508    221,177.841 232.334,177.841 232.334,189.175 243.666,189.175 243.666,177.841 255,177.841  "
-                />
-              </g>
-              <g>
-                <polygon
-                  fill="#000"
-                  points="297.5,166.508 286.166,166.508 286.166,155.175 274.834,155.175 274.834,166.508 263.5,166.508    263.5,177.841 274.834,177.841 274.834,189.175 286.166,189.175 286.166,177.841 297.5,177.841  "
-                />
-              </g>
-            </svg>
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "js"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "python"]} size="3x" />
+          </SkillWrapper>
+          <div className="flex-1">
+            <CPlusPlus />
+          </div>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "js"]} size="3x" />
+          </SkillWrapper>
         </SkillCard>
         <SkillCard title="Frontend">
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "html5"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "css3"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "bootstrap"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "react"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "vuejs"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "wordpress"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "html5"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "css3"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "react"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "vuejs"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "wordpress"]} size="3x" />
+          </SkillWrapper>
         </SkillCard>
         <SkillCard title="Backend">
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "node-js"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "python"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fas", "gem"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4 text-center">
-            <FontAwesomeIcon
-              icon={["fab", "php"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "node-js"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "python"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fas", "gem"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "php"]} size="3x" />
+          </SkillWrapper>
         </SkillCard>
         <SkillCard title="Miscellaneous">
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "linux"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
-          <span className="mx-4">
-            <FontAwesomeIcon
-              icon={["fab", "aws"]}
-              size="2x"
-              className="block mx-auto"
-            />
-          </span>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "linux"]} size="3x" />
+          </SkillWrapper>
+          <SkillWrapper>
+            <FontAwesomeIcon icon={["fab", "aws"]} size="3x" />
+          </SkillWrapper>
         </SkillCard>
         <SkillCard title="Scientific Software">
-          <span className="mx-4">LAMMPS</span>
-          <span className="mx-4">JDFTx</span>
-          <span className="mx-4">Matplotlib</span>
-          <span className="mx-4">MATLAB</span>
+          <span>
+            Molecular Dynamics &middot; Density Functional Theory &middot;
+            Scientific Plots with Matplotlib &middot; MATLAB
+          </span>
         </SkillCard>
       </div>
-      <div className="text-right p-2">
-        Built with React &amp; Tailwind. Hosted on Netlify.
+      <div className="text-center text-xs p-2">
+        Built with React &amp; Tailwind. <br /> Hosted on Netlify.
       </div>
     </div>
   );
