8b869a47e517b62ca0d9755cac3c48a68ba578e9
abhishandy <abhishek.sandy@gmail.com>
2023-07-29 22:52:30 -0400

    DNS records

--- infra/App.ts
+++ infra/App.ts
@@ -7,7 +7,13 @@ import {
   CloudFrontWebDistribution,
   ViewerCertificate,
 } from "aws-cdk-lib/aws-cloudfront";
-import { HostedZone } from "aws-cdk-lib/aws-route53";
+import {
+  ARecord,
+  AaaaRecord,
+  HostedZone,
+  RecordTarget,
+} from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
 import {
   BlockPublicAccess,
   Bucket,
@@ -22,12 +28,14 @@ class MyStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const domainName = "abhishandy.com";
+
     const hostedZone = HostedZone.fromLookup(this, "HostedZone", {
-      domainName: "abhishandy.com",
+      domainName,
     });
 
     const SSLCert = new Certificate(this, "SSLCert", {
-      domainName: "abhishandy.com",
+      domainName,
       validation: CertificateValidation.fromDns(hostedZone),
     });
 
@@ -45,7 +53,7 @@ class MyStack extends Stack {
       "Cloudfront",
       {
         viewerCertificate: ViewerCertificate.fromAcmCertificate(SSLCert, {
-          aliases: ["abhishandy.com"],
+          aliases: [domainName],
         }),
         errorConfigurations: [
           {
@@ -70,6 +78,22 @@ class MyStack extends Stack {
         ],
       }
     );
+
+    new ARecord(this, "ARecord", {
+      recordName: domainName,
+      target: RecordTarget.fromAlias(
+        new CloudFrontTarget(cloudfrontDistribution)
+      ),
+      zone: hostedZone,
+    });
+
+    new AaaaRecord(this, "AaaaRecord", {
+      recordName: domainName,
+      target: RecordTarget.fromAlias(
+        new CloudFrontTarget(cloudfrontDistribution)
+      ),
+      zone: hostedZone,
+    });
 
     new BucketDeployment(this, "ClientBucketDeployment", {
       destinationBucket: bucket,
